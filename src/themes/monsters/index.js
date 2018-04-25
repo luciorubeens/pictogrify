2d3fb839604bd3175b892d918407246d52c2b54e
Lucio Rubens <luciorubeens@gmail.com>
2018-04-25 18:59:57 -0300

    Add monsters theme by Sliday

---
+++ src/themes/monsters/index.js
@@ -0,0 +1,76 @@
+module.exports = {
+  'name': 'Monsters',
+  'viewBox': '0 0 300 300',
+  'shapes': {
+    'body': {
+      'length': 3,
+      'fill': 'skin',
+      'currentColor': '#3776fd'
+    },
+    'eyebrow': {
+      'length': 5,
+    },
+    'ear': {
+      'length': 4,
+      'fill': 'skin',
+      'currentColor': '#3776fd'
+    },
+    'eye': {
+      'length': 9,
+    },
+    'horn': {
+      'length': 9,
+      'fill': 'hair',
+      'currentColor': '#402740'
+    },
+    'mouth': {
+      'length': 9
+    },
+    'nose': {
+      'length': 5,
+    }
+  },
+  'colors': {
+    'background': [
+      '#B1E2FF',
+      '#A7FFC4',
+      '#FFF0DE',
+      '#FEA47F',
+      '#FFFFB1',
+      '#74B9FF',
+      '#A29BFE',
+      '#E6E6E6',
+    ],
+    'clothing': [
+      '#006266',
+      '#5758BB',
+      '#833471',
+      '#3C40C6',
+      '#F79F1F',
+      '#009432',
+      '#CD6133',
+      '#A3CB38'
+    ],
+    'hair': [
+      '#A55728',
+      '#2C1B18',
+      '#B58143',
+      '#D6B370',
+      '#724133',
+      '#4A312C',
+      '#F59797',
+      '#ECDCBF',
+      '#C93305',
+      '#1F3540'
+    ],
+    'skin': [
+      '#FD9841',
+      '#F8D25C',
+      '#FFDBB4',
+      '#FFDBB4',
+      '#D08B5B',
+      '#AE5D29',
+      '#614335'
+    ]
+  }
+}
